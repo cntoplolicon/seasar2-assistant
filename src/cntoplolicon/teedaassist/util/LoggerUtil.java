e8433189e22e52be3692d0244c510d7ca181a396
cntoplolicon <cntoplolicon@gmail.com>
2013-11-27 15:52:17 +0800

    add logger util

---
+++ src/cntoplolicon/teedaassist/util/LoggerUtil.java
@@ -0,0 +1,53 @@
+package cntoplolicon.teedaassist.util;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+
+import cntoplolicon.teedaassist.Activator;
+
+public class LoggerUtil {
+
+	public static final void info(String message) {
+		Activator.getDefault().getLog().log(new Status(IStatus.INFO, Activator.PLUGIN_ID, message));
+	}
+
+	public static final void info(Throwable t) {
+		Activator.getDefault().getLog()
+				.log(new Status(IStatus.INFO, Activator.PLUGIN_ID, t.getMessage(), t));
+	}
+
+	public static final void info(String message, Throwable t) {
+		Activator.getDefault().getLog()
+				.log(new Status(IStatus.INFO, Activator.PLUGIN_ID, message, t));
+	}
+
+	public static final void warn(String message) {
+		Activator.getDefault().getLog()
+				.log(new Status(IStatus.WARNING, Activator.PLUGIN_ID, message));
+	}
+
+	public static final void warn(Throwable t) {
+		Activator.getDefault().getLog()
+				.log(new Status(IStatus.WARNING, Activator.PLUGIN_ID, t.getMessage(), t));
+	}
+
+	public static final void warn(String message, Throwable t) {
+		Activator.getDefault().getLog()
+				.log(new Status(IStatus.WARNING, Activator.PLUGIN_ID, message, t));
+	}
+
+	public static final void error(String message) {
+		Activator.getDefault().getLog()
+				.log(new Status(IStatus.ERROR, Activator.PLUGIN_ID, message));
+	}
+
+	public static final void error(Throwable t) {
+		Activator.getDefault().getLog()
+				.log(new Status(IStatus.ERROR, Activator.PLUGIN_ID, t.getMessage(), t));
+	}
+
+	public static final void error(String message, Throwable t) {
+		Activator.getDefault().getLog()
+				.log(new Status(IStatus.ERROR, Activator.PLUGIN_ID, message, t));
+	}
+}
